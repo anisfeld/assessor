63dade6110dd07b5ff5cf7e61c363bfd1d2616b3
Ari <anisfeld@uchicago.edu>
2018-02-03 13:46:33 -0600

    draft2

--- combine_data.R
+++ combine_data.R
@@ -19,6 +19,23 @@ st_write(zips, "zips.")
 
 st_write(zd, "zd.csv", layer_options = "GEOMETRY=AS_WKT", delete_dsn = TRUE)
 
+
+data <- read_csv("./Downloads/zd.csv")
+
+
+d <- data %>% mutate("Percentage African-American or Hispanic"=paste0(round(black+hispanic, 3)*100, "%"),
+                      "Median Sale Price" = paste0("$", value),
+                      "Median Household Income" = paste0("$", round(medhinc, -2)),
+                      "Final Market Value Ratio" = round(ratio,3),
+                      "First pass Market Value Raio" = round(ratio1,3),
+                      "Number of Homes Sold" = N
+                                      ) %>% View
+write_csv(d, 'geo_export_byshortzip_after2010_edit_aa_20180203.csv')
+
+
+
+
+
 class(zd)
 names(zips)
 
